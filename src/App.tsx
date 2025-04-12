84816d18a035be5b3b101723ba98e8c99918a8e2
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2025-04-12 21:59:31 +0530

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Question as QuestionType, QuestionResponse, UserAnswer } from './types';
 import Question from './components/Question';
 import Feedback from './components/Feedback';
